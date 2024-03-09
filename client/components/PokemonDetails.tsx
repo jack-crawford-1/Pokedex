a60c8266393107a209e61cc8c587b4da968693e6
Jack Crawford <primes_unit.05@icloud.com>
2024-03-09 13:48:39 +1300

    added individual pokemon data to each pokemon page

--- client/components/PokemonDetails.tsx
+++ client/components/PokemonDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Pokemon } from '../../models/Pokemon.ts'
 import { useQuery } from '@tanstack/react-query'
 import { fetchPokemonByName } from '../apis/Pokemon.ts'
@@ -7,7 +7,6 @@ export default function PokemonDetail() {
   const { pokemonName } = useParams()
 
   if (pokemonName === undefined) {
-    console.log(pokemonName)
     return <p>Pokemon name not provided. Please specify a Pokemon.</p>
   }
 
@@ -27,11 +26,33 @@ export default function PokemonDetail() {
     return <p>Not working</p>
   }
 
-  console.log(pokemonName)
-
   return (
     <div>
+      <button>
+        <Link to="/">Home</Link>
+      </button>
       <h1>{pokemonName}</h1>
+      <h2>Index</h2>
+      <p>{pokemon.id}</p>
+      <img src={pokemon.sprites.front_default} alt={pokemonName} />
+      <h2>Types</h2>
+      <ul>
+        {pokemon.types.map((type) => (
+          <li key={type.type.name}>{type.type.name}</li>
+        ))}
+      </ul>
+      <h2>Abilities</h2>
+      <ul>
+        {pokemon.abilities.map((ability) => (
+          <li key={ability.ability.name}>{ability.ability.name}</li>
+        ))}
+      </ul>
+      <h2>Moves</h2>
+      <ul>
+        {pokemon.moves.map((move) => (
+          <li key={move.move.name}>{move.move.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
